d6e8d31dfe4e26b284511d34f9a2ff3ca385a976
OnixFang <twily.dev@outlook.com>
2021-04-30 10:37:17 -0400

    Add basic server code

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("yes");
+// Modules
+const express = require('express');
+
+// App config
+const app = express();
+const { HOST, PORT } = process.env;
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('OK');
+});
+
+// Server launch
+app.listen(PORT, HOST, () => {
+  console.log(`App listening on http://${HOST}:${PORT}/`);
+});
