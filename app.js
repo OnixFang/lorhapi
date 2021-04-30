0280e56cd7f89ec09f8674bb9ac7aa1ed36d379f
OnixFang <twily.dev@outlook.com>
2021-04-30 12:16:56 -0400

    Setup server app to connect to database and use routes

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 // Modules
 const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
 
 // App config
+const { HOST, PORT, MONGODB_URL } = process.env;
 const app = express();
-const { HOST, PORT } = process.env;
+app.use(express.json());
+
+// DB Connection
+mongoose.connect(
+  MONGODB_URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  },
+  (err) => {
+    if (err) {
+      throw err;
+    }
+    console.log('Successfully connected to dabatase.');
+  }
+);
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('OK');
-});
+app.use(routes);
 
 // Server launch
 app.listen(PORT, HOST, () => {
